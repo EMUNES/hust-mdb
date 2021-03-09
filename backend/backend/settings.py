4b02da68ba4858bc43c1f8d257b18517fcdc5584
hzb <hzb123hhh@gmail.com>
2021-03-09 19:56:30 +0800

    django use sqlite

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,13 +140,15 @@ REST_FRAMEWORK = {
 # Use mysql as the database.
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sample', # TODO: change here
-        'USER': 'root',
-        'PASSWORD': "HZB123hhh",
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        ### MySQL can't import csv file correctly.
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'sample',
+        # 'USER': 'root',
+        # 'PASSWORD': "HZB123hhh",
+        # 'HOST': 'localhost',
+        # 'PORT': '3306'
     }
 }
