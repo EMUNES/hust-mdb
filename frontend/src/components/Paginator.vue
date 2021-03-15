6bf9e1000d1a54a285f912a9c9f2e9a888f3a9cc
hzb <hzb123hhh@gmail.com>
2021-03-15 12:29:00 +0800

    paginator add placeholde

--- frontend/src/components/Paginator.vue
+++ frontend/src/components/Paginator.vue
@@ -23,7 +23,7 @@
             <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
           </svg>
         </div>
-        <input type="text" v-show="jump" v-model="destPage" @keypress.enter="finishJump(destPage)"
+        <input type="text" v-show="jump" v-model="destPage" @keypress.enter="finishJump(destPage)" placeholder="输入"
         class="h-5 w-8">
       </a>
       <a href="javascript: void(0)" @click="pageTo(pageCurrent)"
@@ -46,7 +46,7 @@
             <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
           </svg>
         </div> 
-        <input type="text" v-show="jump" v-model="destPage" @keypress.enter="finishJump(destPage)"
+        <input type="text" v-show="jump" v-model="destPage" @keypress.enter="finishJump(destPage)" placeholder="输入"
         class="h-5 w-8">
       </a>
       <a href="javascript: void(0)" v-if="pageLast1" @click="pageTo(pageLast1)"
