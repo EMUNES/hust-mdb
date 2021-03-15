05c3073c4ecf3771e52643ae879cfcbe78847c3e
hzb <hzb123hhh@gmail.com>
2021-03-15 16:29:24 +0800

    update ignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 # Use mysql as the database.
