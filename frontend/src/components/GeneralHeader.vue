5d65602efcc9b9d7488292db4e8c2e6bd575b56b
hzb <hzb123hhh@gmail.com>
2021-04-02 20:10:11 +0800

    implement logout

--- frontend/src/components/GeneralHeader.vue
+++ frontend/src/components/GeneralHeader.vue
@@ -1,5 +1,19 @@
 <template>
-  <header class="bg-blue-200 pt-14 pb-10">
+  <div class="flex fixed items-center"> 
+    <span v-if="$store.getters.isAuthenticated"
+    class="mt-3 mx-3">
+      已登录
+    </span>
+    <button v-if="!$store.getters.isAuthenticated"
+    class="mt-3 mx-1 px-1 border-2 border-blue-200 rounded">
+      登陆
+    </button>
+    <button @click="logout"
+    class="mt-3 mx-1 px-1 border-2 border-blue-200 rounded">
+      登出
+    </button>
+  </div>
+  <header class="bg-blue-100 pt-14 pb-10">
     <h2 class="text text-3xl mb-5">
       华中科技大学
     </h2>
@@ -12,11 +26,28 @@
       热塑性材料数据库
     </h2>
   </header>
+
 </template>
 
 <script>
+import { useStore } from 'vuex'
+import { useRouter} from 'vue-router'
+
 export default {
-  name: "GeneralHeader"
+  name: "GeneralHeader",
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    const logout = () => {
+      localStorage.removeItem(store.state.localTokenName)
+      router.push('/login')
+    }
+
+    return {
+      logout
+    }
+  }
 }
 </script>
 
