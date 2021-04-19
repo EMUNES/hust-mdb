571dd444a6e0bda1d4858f85426a176c1aa05ec4
hzb <hzb123hhh@gmail.com>
2021-04-19 12:00:46 +0800

    add transition for login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="bg-hust-bg-full bg-cover">
-    <header class="fixed my-10 mx-10 text-3xl">
-      华中科技大学
-    </header>
+  <transition appear
+  enter-active-class="transition duration-700 ease-out"
+  enter-from-class="opacity-0 scale-75"
+  enter-to-class="opacity-100 scale-100">
+    <div class="bg-hust-bg-full bg-cover">
+      <header class="fixed my-10 mx-10 text-3xl">
+        华中科技大学
+      </header>
 
-    <div class="flex flex-col items-center justify-center h-screen w-2/3 md:m-auto max-w-3xl">
-      <div class="mb-10">
-        <button class="text-blue-400 border-b-2 border-current px-5 text-xl mx-1">
-          登录
-        </button>
-        
-        <button class="hover:text-green-600 border-b-2 hover:border-current px-5 text-xl mx-1">
-          注册
-        </button>
+      <div class="flex flex-col items-center justify-center h-screen w-2/3 md:m-auto max-w-3xl">
+        <div class="mb-10">
+          <button class="text-blue-400 border-b-2 border-current px-5 text-xl mx-1">
+            登录
+          </button>
+          
+          <button class="hover:text-green-600 border-b-2 hover:border-current px-5 text-xl mx-1">
+            注册
+          </button>
+        </div>
+        <login-form @login-submit="login" class="opacity-90">
+
+        </login-form>
       </div>
-      <login-form @login-submit="login" class="opacity-90">
-
-      </login-form>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
