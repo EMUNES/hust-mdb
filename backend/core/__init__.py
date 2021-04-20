d7d6197c6fb3df5ed604babaed5ae952c516f97f
hzb <hzb123hhh@gmail.com>
2021-04-20 10:12:14 +0800

    update doc for applications

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,2 +1,5 @@
 """Allow users to view and modify (add, delete, chnage) material data.
+
+For the convennience of data manipulation and displaying, this application
+is built on Django REST Framework.
 """
